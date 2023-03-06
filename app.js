a885a6d7a54b7ae1a3bba05dbeddaa5e7293a67d
Fernanda Leite <contato@nandaleite.com>
2023-03-06 20:22:22 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { key: "h1", className: "title" }, "React IS rendered"),
+    React.createElement(Person, { name: "David Lee", occupation: "Chef" }, null),
+    React.createElement(Person, { name: "Rachel Chen", occupation: "Journalist" }, null),
+    React.createElement(Person, { name: "Liam Johnson", occupation: "Financial Analyst" }, null),
+  ]);
+};
+
+const Person = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.occupation),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
